19c18444c801aa689f727a6c8f94ae7014aaec2c
Darrel Abello <netzon.darrel@gmail.com>
2016-09-26 14:00:15 +0800

    Controllers >  BaseApiController: Initial code drop

---
+++ WebApplication3/Controllers/BaseApiController.cs
@@ -0,0 +1,46 @@
+﻿using ErrorCatcher.Interface;
+using ErrorCatcher.Methods;
+using ErrorCatcher.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+
+namespace WebApplication3.Controllers
+{
+    public class BaseApiController: ApiController
+    {
+        public ISendCustomError customResponseMsgService = new SendCustomResponse();
+
+        public HttpResponseMessage InvalidModelState(ModelStateDictionary modelState)
+        {
+            List<Error> errors = new List<Error>();
+
+            foreach (var item in modelState)
+            {
+                errors.Add(new Error()
+                {
+                    Message = "The request is invalid",
+                    Reason = $"[{item.Key}] - {item.Value}",
+                    Domain = Request.RequestUri.AbsoluteUri                    
+                });
+            }
+
+            return customResponseMsgService
+                .ReturnResponse(Request, 
+                                HttpStatusCode.BadRequest, 
+                                "The request is invalid", 
+                                errors);
+        }
+
+        public HttpResponseMessage StatusOk<T>(T arg)
+        {
+            return Request.CreateResponse(HttpStatusCode.OK, arg);
+        }
+
+    }
+}
